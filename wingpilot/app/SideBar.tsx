53dd83ea75bbf079a98d8b66c7dd0a993ef4d610
minatofujimoto <minatofujimototrc@gmail.com>
2025-11-15 21:59:39 +0900

    feat: add unit conversion shortcuts

--- wingpilot/app/SideBar.tsx
+++ wingpilot/app/SideBar.tsx
@@ -68,3 +68,4 @@ export function DefaultSidebar() {
 // Unit feature implementation - 20251105_0012
 // Unit feature implementation - 20251112_0031
 // Unit feature implementation - 20251113_0032
+// Unit feature implementation - 20251115_0038
