ef2cf0cbd009560bec737484c5a0f1dcd5ed6479
minatofujimoto <minatofujimototrc@gmail.com>
2025-11-19 13:16:54 +0900

    test: add unit tests for conversion functions

--- wingpilot/app/page.tsx
+++ wingpilot/app/page.tsx
@@ -32,3 +32,4 @@ export default function Home() {
 // Unit feature implementation - 20251102_0006
 // Unit feature implementation - 20251115_0037
 // Unit feature implementation - 20251117_0044
+// Unit feature implementation - 20251119_0047
