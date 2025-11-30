d26e9873d7d54221474f62d30efeb760a360d368
minatofujimoto <minatofujimototrc@gmail.com>
2025-11-30 18:40:58 +0900

    refactor: optimize unit conversion performance

--- wingpilot/app/Table/page.tsx
+++ wingpilot/app/Table/page.tsx
@@ -229,3 +229,4 @@ export default Table;
 // Unit feature implementation - 20251107_0017
 // Unit feature implementation - 20251121_0053
 // Unit feature implementation - 20251122_0055
+// Unit feature implementation - 20251130_0077
