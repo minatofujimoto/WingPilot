33d5c6d8e109dd4e1cb3eb74cd0e811c08950c56
minatofujimoto <minatofujimototrc@gmail.com>
2025-12-03 18:24:58 +0900

    feat: add unit conversion documentation

--- wingpilot/app/Table/Form.tsx
+++ wingpilot/app/Table/Form.tsx
@@ -179,3 +179,4 @@ export default Form;
 // Unit feature implementation - 20251129_0073
 // Unit feature implementation - 20251129_0075
 // Unit feature implementation - 20251130_0076
+// Unit feature implementation - 20251203_0086
